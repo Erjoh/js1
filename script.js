18f5375b5ba6f036a353279da9a092ef6ac0a07e
Erjoh <erjik1902@gmail.com>
2023-07-10 12:44:16 +0600

    firstjslesson done

---
+++ script.js
@@ -0,0 +1,49 @@
+// let a = 4
+// let b = 55
+//
+// console.log(a + b)
+
+// let a = 4
+// let b = 55
+//
+// console.log(a * b)
+
+// let a = 4
+// let b = 55
+//
+// console.log(a / b)
+
+// let a = 4
+// let b = 55
+//
+// console.log(a % b)
+
+// let a = 'hello'
+// let b = 'Erjan'
+//
+// console.log(a + ' ' + b)
+
+// let a = 3
+// let b = '4fsf'
+//
+// console.log(a / b)
+
+// let a = 5
+// let b = '5'
+//
+// console.log(a + +b)
+
+// let a = 5
+// let b = '5'
+//
+// console.log(a - -b)
+
+// let a = 5
+// let b = '5'
+//
+// console.log(a + Number(b))
+
+// let c = 2
+// let b
+//
+// console.log(typeof b)
